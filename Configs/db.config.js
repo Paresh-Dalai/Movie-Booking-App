fe98771ee652bde05a6ab0e1a8d553bc02dfc454
paresh-dalai <amarjeetdalai17@gmail.com>
2023-07-22 17:32:20 +0530

    Mongo_DB_URL_added

--- Configs/db.config.js
+++ Configs/db.config.js
@@ -4,5 +4,7 @@
 
 module.exports = {
     DB_NAME : "Movie_DB",
-    DB_URL : "mongodb://127.0.0.1/Movie_DB"
+    // DB_URL : "mongodb://127.0.0.1/Movie_DB"
+    DB_URL : "mongodb+srv://Paresh:Paresh123@movie.djhkcn6.mongodb.net/"
 }
+
